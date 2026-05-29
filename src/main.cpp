87af666aa487633927de08b3c0ec4924b9777277
errfct <veronika.sokolova.07@mail.ru>
2026-05-29 15:51:37 +0300

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,16 @@
 // Copyright 2021 NNTU-CS
+#include <iostream>
 #include "bst.h"
 
+void makeTree(BST<std::string>& tree, const char* filename);
+void printFreq(BST<std::string>& tree);
+
 int main() {
-    return 0;
+  BST<std::string> tree;
+  makeTree(tree, "src/war_peace.txt");
+  std::cout << "Tree depth: "
+      << tree.depth() << std::endl;
+  printFreq(tree);
+
+  return 0;
 }
